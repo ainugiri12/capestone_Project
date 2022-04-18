3cd4534647689b04da64a16cb989d7777665bde6
Giri prasad <ainugiri@gmail.com>
2022-04-18 10:26:19 +0530

    initial commit

---
+++ Classname.java
@@ -0,0 +1,6 @@
+class Classname{
+	public static void main(String giri[]){
+			System.out.println("Hello World");
+	}
+}
+		dfasdfasdfasfas
---
+++ Test01.java
@@ -0,0 +1,5 @@
+class Test01{
+public static void main(String args[]){
+System.out.println("JFSD Batch 06");
+}
+}
